daff4e3df10d1fdcd17475bf8f46575516eb518d
Mikhail8815 <mishutka150588@mail.ru>
2025-07-29 20:52:12 +0300

    React query + getStaticProps

--- pages/locations/index.tsx
+++ pages/locations/index.tsx
@@ -1,16 +1,26 @@
 import {LocationType, ResponseType} from "../../assets/api/rick-and-morty-api";
 import {Header} from "../../components/Header/Header";
 import {PageWrapper} from "../../components/PageWrapper/PageWrapper";
-import {useQuery} from "@tanstack/react-query";
-
-// export const getStaticProps = async () => {
-//
-// }
+import {dehydrate, useQuery} from "@tanstack/react-query";
+import {QueryClient} from "@tanstack/query-core";
 
 const getLocations = () => {
     return fetch('https://rickandmortyapi.com/api/location', {
         method: "GET",
     }).then(res => res.json())
+}
+
+export const getStaticProps = async () => {
+
+    const queryClient = new QueryClient();
+
+    await queryClient.fetchQuery(['locations'], getLocations);
+
+    return {
+        props: {
+            dehydratedState: dehydrate(queryClient)
+        }
+    }
 }
 
 const Locations = () => {
